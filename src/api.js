13dc2b29e58ead1dbfaaaf9427e3d13694a685e9
Mario Llamas Lanza <maclanzallamas@hotmail.com>
2020-04-17 15:45:03 +0100

    Add controllers for player endpoints

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const api = Router();
+const { getPlayers, createPlayer, getPlayerById, armPlayer, killPlayer } = require('./controllers/players');
 
 // Creating generic functions for managing the services and errors
-const successHandler = (response) => (data) => response.json(data);
-const errorHandler = (response) => (error, message) => response.status(400).json({ error, message });
+const responseHandler = (response) => (statusCode, data) => response.status(statusCode).json(data);
 
 // Creation of a generic function to handle all endpoints. This helps to extract the body or params 
 // for all the requests and also to have a try catch that will "protect us" returning a 500 error in case something unexpected happens
@@ -12,21 +12,20 @@ const handle = (service) => async (request, response) => {
   try {
     await service(
       data,
-      successHandler(response),
-      errorHandler(response),
+      responseHandler(response),
      );
   } catch (error) {
     console.error(`Unhandled Exception - ${request.url}: `, error);
     console.error(JSON.stringify(data));
-    errorHandler(response)('UNHANDLED_EXCEPTION');
+    responseHandler(response)(500,'UNHANDLED_EXCEPTION');
   }
 };
 
-api.get('/players', (req, res) => handle()(req,res));
-api.post('/player', (req, res) => handle()(req,res));
-api.get('/player/:id', (req, res) => handle()(req,res));
-api.patch('/armPlayer/:id', (req, res) => handle()(req,res));
-api.patch('/killPlayer/:id', (req, res) => handle()(req,res));
+api.get('/players', (req, res) => handle(getPlayers)(req,res));
+api.post('/player', (req, res) => handle(createPlayer)(req,res));
+api.get('/player/:id', (req, res) => handle(getPlayerById)(req,res));
+api.patch('/armPlayer/:id', (req, res) => handle(armPlayer)(req,res));
+api.patch('/killPlayer/:id', (req, res) => handle(killPlayer)(req,res));
 api.post('/createObject', (req, res) => handle()(req,res));
 api.get('/object/:id', (req, res) => handle()(req,res));
 api.put('/upgradeObject/:id', (req, res) => handle()(req,res));
